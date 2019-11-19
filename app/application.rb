e20057b2b21157163136d84de9acadf56eb018a4
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-19 03:13:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,13 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
+    
     if Time.now.to_i < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
+    
+    resp.finish
   end
 end
